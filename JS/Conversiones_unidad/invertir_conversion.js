9c809bf578acb06affd8b5ba3be3ae9ff8bdc920
leonardo <leonardo@gmail.com>
2024-06-02 20:39:53 -0600

    conversiones agregadas :D

---
+++ JS/Conversiones_unidad/invertir_conversion.js
@@ -0,0 +1,29 @@
+function invertirUnidadIngresada(){
+    let inversor=document.getElementsByClassName('simbolo-flechas')[0]
+    let inputs;
+    
+    inversor.addEventListener('click',function cambiarLugares(){
+        inputs=document.getElementsByClassName('input') 
+        let aux;
+
+        console.log('in',inputs); 
+        for(let index=0;index<selects.length-2;index++){
+            console.log(selects[index].value+'\n'+selects[index+2]+'\n\n___');
+            aux=selects[index].value
+            selects[index].value=selects[index+2].value 
+            selects[index+2].value=aux
+            activarEvChangeIn(selects[index])
+            activarEvChangeIn(selects[index+2])
+        }
+
+    }) 
+}
+function cambiarOptionSelected(select_object,option_index){
+    select_object.value=select_object.options[option_index].value
+    activarEvChangeIn(select_object)
+}
+function activarEvChangeIn(select_object){
+    select_object.dispatchEvent(new Event('change'))
+}
+
+invertirUnidadIngresada()
