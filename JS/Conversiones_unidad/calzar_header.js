98c58815a987374fe5ca9737fcf9c32a50bd37b9
Your Name <you@example.com>
2024-06-03 08:38:03 -0600

    header añadido a conversion gay

---
+++ JS/Conversiones_unidad/calzar_header.js
@@ -0,0 +1,17 @@
+let target='void'
+document.body.onresize=function (){
+    // console.error('cambio el header')
+    cambiarTamaño(document.body)
+    // console.log();
+}
+
+function cambiarTamaño(target){
+    let header=document.getElementsByClassName('header')[0]
+    target.style.marginTop=header.clientHeight+"px"
+}
+function iniciarResize(){
+    
+    let body=document.getElementsByTagName('body')[0]
+    cambiarTamaño(body)
+}
+iniciarResize()
